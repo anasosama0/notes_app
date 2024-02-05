6617bb92802f412efd38fe89d2cd74d5c31b448b
anasosama0 <anasosama014@gmail.com>
2024-02-05 05:59:45 +0200

    creat hive and init Hive

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,3 +1,4 @@
 import 'package:flutter/animation.dart';
 
 const kPrimaryColor = Color(0xff82DED1);
+const kNotesBox = 'notes_box';
--- lib/models/note_model.dart
+++ lib/models/note_model.dart
@@ -1,13 +1,23 @@
-class NodeModel{
+import 'package:hive/hive.dart';
+
+
+part 'note_model.g.dart';
+
+@HiveType(typeId: 0)
+class NodeModel extends HiveObject {
+  @HiveField(0)
   final String title;
+  @HiveField(1)
   final String subTitle;
+  @HiveField(2)
   final String date;
+  @HiveField(3)
   final int color;
 
-NodeModel( {
-  required this.title,
-  required this.subTitle, 
-  required this.date, 
-  required this.color,
-});
+  NodeModel({
+    required this.title,
+    required this.subTitle,
+    required this.date,
+    required this.color,
+  });
 }
