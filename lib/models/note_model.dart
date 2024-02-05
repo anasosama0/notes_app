c60e04ef5fbec2bf3b1318362489091e07d18d1c
anasosama0 <anasosama014@gmail.com>
2024-02-05 05:07:28 +0200

    create nodeModel

---
+++ lib/models/note_model.dart
@@ -0,0 +1,13 @@
+class NodeModel{
+  final String title;
+  final String subTitle;
+  final String date;
+  final int color;
+
+NodeModel( {
+  required this.title,
+  required this.subTitle, 
+  required this.date, 
+  required this.color,
+});
+}
