431c4d351186484080330ba41d38bd70e7a80ea9
anasosama0 <anasosama014@gmail.com>
2024-02-06 13:33:11 +0200

    create add note cubit

--- lib/views/widgets/note_list_view.dart
+++ lib/views/widgets/note_list_view.dart
@@ -3,23 +3,26 @@ import 'note_item.dart';
 
 class NotesListView extends StatelessWidget {
   const NotesListView({super.key});
-final data = const [
-  Color(0xffFFCD7B),
-  Color(0xffE8E897),
-  Color(0xff77D6EF),
-  Color(0xffD19DD9)
-];
+  final data = const [
+    Color(0xffFFCD7B),
+    Color(0xffE8E897),
+    Color(0xff77D6EF),
+    Color(0xffD19DD9)
+  ];
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: EdgeInsets.zero,
-      child: ListView.builder(itemBuilder: (context, index) {
-        return  Padding(
-          padding:  const EdgeInsets.symmetric(vertical: 4),
-          child: NoteItem(backgroundColor: data[index],),
-        );
-      },
-      itemCount: data.length,
+      child: ListView.builder(
+        itemBuilder: (context, index) {
+          return Padding(
+            padding: const EdgeInsets.symmetric(vertical: 4),
+            child: NoteItem(
+              backgroundColor: data[index],
+            ),
+          );
+        },
+        itemCount: data.length,
       ),
     );
   }
