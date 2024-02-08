a55a9c1174beee0f695caaccf452d8176e77bbd4
anasosama0 <anasosama014@gmail.com>
2024-02-08 02:35:11 +0200

    display data part two

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/animation.dart';
 
 const kPrimaryColor = Color(0xff82DED1);
+
 const kNotesBox = 'notes_box';
--- lib/views/widgets/note_item.dart
+++ lib/views/widgets/note_item.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:notes_app/models/note_model.dart';
 import 'package:notes_app/views/edit_note_view.dart';
 
 class NoteItem extends StatelessWidget {
-  const NoteItem({super.key});
+  const NoteItem({super.key,required this.note});
 
+final NoteModel note;
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
@@ -15,19 +17,21 @@ class NoteItem extends StatelessWidget {
       },
       child: Container(
         padding: const EdgeInsets.only(left: 16, top: 24, bottom: 24),
-        decoration: BoxDecoration(borderRadius: BorderRadius.circular(12)),
+        decoration: BoxDecoration(
+          color: Color(note.color),
+          borderRadius: BorderRadius.circular(12)),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.end,
           children: [
             ListTile(
-              title: const Text(
-                'Flutter Tips',
-                style: TextStyle(color: Colors.black, fontSize: 26),
+              title:  Text(
+                note.title,
+                style: const TextStyle(color: Colors.black, fontSize: 26),
               ),
               subtitle: Padding(
                 padding: const EdgeInsets.only(top: 16, bottom: 16),
                 child: Text(
-                  'build your career with Tharwat Samy',
+                  note.subTitle,
                   style: TextStyle(
                       fontSize: 18, color: Colors.black.withOpacity(.5)),
                 ),
@@ -43,7 +47,7 @@ class NoteItem extends StatelessWidget {
             Padding(
               padding: const EdgeInsets.only(right: 24),
               child: Text(
-                'May21, 2022',
+                note.date,
                 style: TextStyle(color: Colors.black.withOpacity(.5)),
               ),
             )
